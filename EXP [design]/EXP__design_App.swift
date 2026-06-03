5aa763a71c7d6910f5ee052dff1fc699eca59b46
tapps <t@thisroad.org>
2026-06-03 10:45:34 -0500

    Initial Commit

---
+++ EXP [design]/EXP__design_App.swift
@@ -0,0 +1,17 @@
+//
+//  EXP__design_App.swift
+//  EXP [design]
+//
+//  Created by tapps on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct EXP__design_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
